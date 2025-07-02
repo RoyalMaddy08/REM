b6c06fdcf78779845e706e0df21a111bac7686ae
RoyalMaddy08 <133629919+RoyalMaddy08@users.noreply.github.com>
2025-07-03 02:06:04 +0530

    third.py

--- dem.py
+++ dem.py
@@ -1,4 +1,3 @@
-
 dem = rioxarray.open_rasterio("/content/USGS_1_n52w119_20130911.tif")
 dem = dem.coarsen(x=3, boundary='trim').mean().coarsen(y=3, boundary='trim').mean()
 dem.squeeze().plot.imshow()

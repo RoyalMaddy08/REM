82b010419b95e6b0db9b54a2e11d1144b8181c66
RoyalMaddy08 <133629919+RoyalMaddy08@users.noreply.github.com>
2025-07-03 02:04:25 +0530

    Create lib.py

---
+++ lib.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from IPython.core.display import Video
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd  # Vector data handling
+import osmnx as ox       # Downloading data from OSM
+
+from shapely.geometry import box
+from scipy.spatial import cKDTree as KDTree # For Inverse Distance Weight calculation
+
+import xarray as xr    
+import xrspatial    # Hillshading
+import rioxarray    # Working with geospatial data in xarray
+
+import matplotlib.pyplot as plt
+from datashader.transfer_functions import shade, stack
